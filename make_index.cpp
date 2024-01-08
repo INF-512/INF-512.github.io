67fb245e58e84e60603c40b0ba709b429534740f
INF-512 <1908897917@qq.com>
2024-01-08 15:54:00 +0800

    updata

--- make_index.cpp
+++ make_index.cpp
@@ -11,18 +11,29 @@ int dfs(fs::path p, int fid) {
   int cid = ++utot;
   // cout << cid << ' ' << fid << ' ' << p.string() << endl;
   cstr += format("<ul id=\"u{}\">\n", cid);
-  cstr += format("<li onclick=\"replace_ul('u{}', 'u{}')\">{}</li>\n", cid, fid, p.stem().string());
-  for (const auto &file : fs::directory_iterator(p)) {
-    // cout << cid << ' ' << file.path() << endl;
-    if (file.is_directory()) {
+  if (cid == 1) {
+    cstr += format("<li onclick=\"replace_ul('u{}', 'u{}')\">{}</li>\n", cid, fid, "x");
+    cstr += format("<li><a href=\"/index.html?src={}?ver={}&pos=u{}\">{}</a></li>\n", "/首页.md", gen(), cid, "首页");
+    int nid;
+    nid = dfs(p / "算法", cid);
+    cstr += format("<li onclick=\"replace_ul('u{}', 'u{}')\">{}</li>\n", cid, nid, "算法>");
+    nid = dfs(p / "数据结构", cid);
+    cstr += format("<li onclick=\"replace_ul('u{}', 'u{}')\">{}</li>\n", cid, nid, "数据结构>");
+    nid = dfs(p / "题目", cid);
+    cstr += format("<li onclick=\"replace_ul('u{}', 'u{}')\">{}</li>\n", cid, nid, "题目>");
+  }
+  else {
+    cstr += format("<li onclick=\"replace_ul('u{}', 'u{}')\">{}</li>\n", cid, fid, "<" + p.stem().string());
+    for (const auto &file : fs::directory_iterator(p)) {
       // cout << cid << ' ' << file.path() << endl;
-      if (cid != 1 || file.path().filename() == "article") {
+      if (file.is_directory()) {
+        // cout << cid << ' ' << file.path() << endl;
         int nid = dfs(file.path(), cid);
-        cstr += format("<li onclick=\"replace_ul('u{}', 'u{}')\">{}</li>\n", cid, nid, file.path().stem().string());
+        cstr += format("<li onclick=\"replace_ul('u{}', 'u{}')\">{}</li>\n", cid, nid, file.path().stem().string()+">");
       }
-    }
-    else if (file.is_regular_file() && file.path().extension() == ".md") {
-      cstr += format("<li><a href=\"/test.html?src={}?ver={}&pos=u{}\">{}</a></li>\n", file.path().string(), gen(), cid, file.path().stem().string());
+      else if (file.is_regular_file() && file.path().extension() == ".md") {
+        cstr += format("<li><a href=\"/index.html?src={}?ver={}&pos=u{}\">{}</a></li>\n", file.path().string(), gen(), cid, file.path().stem().string());
+      }
     }
   }
   cstr += "</ul>\n";
