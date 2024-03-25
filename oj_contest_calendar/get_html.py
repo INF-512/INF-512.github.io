942c3305d8ab51c22c233e461cd583651f01e6f3
INF-512 <1908897917@qq.com>
2024-03-26 07:56:24 +0800

    updata

--- oj_contest_calendar/get_html.py
+++ oj_contest_calendar/get_html.py
@@ -2,9 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 # import io
 import sys
+import time
 
 def get_html(url, file_name, driver) :
+  driver.set_window_size(2000,2000)
   driver.get(url)
+  # driver.execute_script("var q=document.documentElement.scrollTop=10000")
+  for i in range(10):
+    driver.execute_script(f'document.documentElement.scrollTop={(i+1)*1000}')
+  time.sleep(1)
   f = open(file_name, 'w', encoding='utf-8')
   f.write(driver.page_source)
   f.close()
